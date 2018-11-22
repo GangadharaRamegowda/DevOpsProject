bbe46b26702cdabbf787c3bb57801ed7fd26fbca
Gangadhara <Gangadhara.Doddarasinakere_Ramegowda@telus.com>
2018-11-22 19:38:34 +0530

    gitignore file added

--- test/TestJunit1.java
+++ test/TestJunit1.java
@@ -1,11 +1,17 @@
 import org.junit.Test;
 import org.junit.Ignore;
+import org.junit.Before;
 import static org.junit.Assert.assertEquals;
 
 public class TestJunit1 {
 
    String message = "Robert";	
    MessageUtil messageUtil = new MessageUtil(message);
+   
+   @Before 
+   public void setUp() {
+      System.out.println("*********TestJunit1 SetUp ****************");    
+   }
    
    @Test
    public void testPrintMessage() {	
