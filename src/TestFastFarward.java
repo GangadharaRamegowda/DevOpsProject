07b26caa6411708166cdf6e14fc9fa3a041620aa
Gangadhara <Gangadharadr@gmail.com>
2019-05-13 23:05:06 +0530

    Added for fast farward Test

--- src/TestFastFarward.java
+++ src/TestFastFarward.java
@@ -2,10 +2,11 @@
 public class TestFastFarward {
 	
 	public static void main(String args[]) {
+		System.out.println("Testing is Started");
 		System.out.println("Shiva");
 		System.out.println("Ganesha");
 		System.out.println("Parvathi");
-		System.out.println("Shanmuka");
+		System.out.println("Nandhi");
 		System.out.println("Testing is End");
 	}
 
