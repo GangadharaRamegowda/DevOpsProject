abff2f6b91028e65d29459f8eebc98dd6d2cdbfd
Gangadhara <Gangadharadr@gmail.com>
2019-05-13 18:55:08 +0530

    Testing

--- src/TestFastFarward.java
+++ src/TestFastFarward.java
@@ -2,19 +2,11 @@
 public class TestFastFarward {
 	
 	public static void main(String args[]) {
-		System.out.println("This is Testing");
 		System.out.println("Shiva");
 		System.out.println("Ganesha");
 		System.out.println("Parvathi");
-		System.out.println("Nandhi");
+		System.out.println("Shanmuka");
+		System.out.println("Testing is End");
 	}
 
-	public static void main(String[] args) {
-	
-		System.out.println("Shiva");
-		System.out.println("Ganesha");
-		System.out.println("Parvathi");
-		System.out.println("Shanmukha");
-		System.out.println("Testing Ends");
-	}
 }
