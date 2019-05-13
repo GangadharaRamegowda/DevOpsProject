8882b30dbe52c58e7ddb38f1dffa457bdaf30045
Gangadhara <Gangadharadr@gmail.com>
2019-05-13 15:43:48 +0530

    commited to local repository

--- src/Test.java
+++ src/Test.java
@@ -3,6 +3,7 @@ public class Test {
 
 public static void main(String args[]) {
 	System.out.println("***********Shiva Ganesha ***********");
+	System.out.println("***********Shiva Ganesha Parvathi ***********");
 }	
 	
 }
