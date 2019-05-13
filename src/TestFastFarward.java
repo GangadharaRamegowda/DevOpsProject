80060622c53dde3d03960e10713f65ed516c9702
Gangadhara <Gangadharadr@gmail.com>
2019-05-13 18:15:03 +0530

    Syso statements added

--- src/TestFastFarward.java
+++ src/TestFastFarward.java
@@ -1,4 +1,12 @@
 
 public class TestFastFarward {
+	
+	public static void main(String args[]) {
+		System.out.println("This is Testing");
+		System.out.println("Shiva");
+		System.out.println("Ganesha");
+		System.out.println("Parvathi");
+		System.out.println("Nandhi");
+	}
 
 }
