4c3c4174a0e9f44485d8ac864db93f805020423c
Gangadhara <Gangadharadr@gmail.com>
2019-05-13 23:17:42 +0530

    Testing

--- src/TestFastFarward.java
+++ src/TestFastFarward.java
@@ -5,8 +5,9 @@ public class TestFastFarward {
 		System.out.println("Shiva");
 		System.out.println("Ganesha");
 		System.out.println("Parvathi");
-		System.out.println("Shanmuka");
+		System.out.println("Skandaswamy");
 		System.out.println("Testing is End");
+		System.out.println("Testing is Wonderfull");
 	}
 
 }
