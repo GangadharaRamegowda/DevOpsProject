496bc920e8644ee94271c85ff6dd30b6ec008855
Gangadhara <Gangadharadr@gmail.com>
2019-05-13 18:28:50 +0530

    updated the file

--- src/TestFastFarward.java
+++ src/TestFastFarward.java
@@ -1,4 +1,12 @@
 
 public class TestFastFarward {
 
+	public static void main(String[] args) {
+	
+		System.out.println("Shiva");
+		System.out.println("Ganesha");
+		System.out.println("Parvathi");
+		System.out.println("Shanmukha");
+		System.out.println("Testing Ends");
+	}
 }
