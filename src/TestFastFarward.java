be0d5473f7dfdf3f0473f280e26eca85b364d366
Gangadhara <Gangadharadr@gmail.com>
2019-05-19 23:54:42 +0530

    Fixed as per the suggestions from Sonar Qube

--- src/TestFastFarward.java
+++ src/TestFastFarward.java
@@ -1,7 +1,7 @@
 
 public class TestFastFarward {
 	
-	public static void main(String args[]) {
+	public static void main(String[] args) {
 		System.out.println("Testing is Started");
 		System.out.println("Shiva");
 		System.out.println("Ganesha");
